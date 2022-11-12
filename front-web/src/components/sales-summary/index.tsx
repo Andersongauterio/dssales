26b5dc1ff5c16c20b7364ce44c0b03e1a3dc587b
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-11-12 20:17:29 -0300

    sales-summary final layout

--- front-web/src/components/sales-summary/index.tsx
+++ front-web/src/components/sales-summary/index.tsx
@@ -1,13 +1,17 @@
 import SalesSummaryCard from './sales-summary-card';
 import './styles.css';
+import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
+import { ReactComponent as BarChartIcon } from '../../assets/bar-chart-icon.svg';
+import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
+import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
 
 const SalesSummary = () => {
   return (
     <div className="sales-summary-container">
-      <SalesSummaryCard />
-      <SalesSummaryCard />
-      <SalesSummaryCard />
-      <SalesSummaryCard />
+      <SalesSummaryCard value={430} label="Média" icon={<DoneIcon />}/>
+      <SalesSummaryCard value={630} label="Quantidade" icon={<SyncIcon />}/>
+      <SalesSummaryCard value={130} label="Mínima" icon={<BarChartIcon />}/>
+      <SalesSummaryCard value={230} label="Máxima" icon={<AvatarIcon />}/>
     </div>
   )
 }
--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
 import './styles.css';
 import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
 
-const SalesSummaryCard = () => {
+type Props = {
+  value: number;
+  label: string;
+  icon: React.ReactNode;
+}
+
+const SalesSummaryCard = ({value, label, icon}: Props) => {
   return (
     <div className="sales-summary-card base-card">
-      <AvatarIcon />
-      <h3 className="sales-summary-card-value">534.00</h3>
-      <span className="sales-summary-card-label">Média</span>
+      {icon}
+      <h3 className="sales-summary-card-value">{value}</h3>
+      <span className="sales-summary-card-label">{label}</span>
     </div>
   )
 };
