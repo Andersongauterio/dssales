9de878f42b61dbc73c22c6e46b2444161ec9fe38
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-11-19 09:59:45 -0300

    Filter integration salesByDate

--- front-web/src/utils/request.ts
+++ front-web/src/utils/request.ts
@@ -1,7 +1,17 @@
+import { FilterData } from './../types';
 import  axios  from "axios";
+import { formatDateToServer } from './formatters';
 
 const baseURL = 'http://localhost:8080';
 
 export const makeRequest = axios.create({
   baseURL
 });
+
+export const buildFilterParams = (FilterData?: FilterData) => {
+  return {
+    minDate: formatDateToServer(FilterData?.dates?.[0]),
+    maxDate: formatDateToServer(FilterData?.dates?.[1]),
+    gender: FilterData?.gender
+  };
+};
